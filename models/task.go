0f036dba7d68b9b147d478cc765617b83043da3c
firqinmarindra <68682942+firqinmarindra@users.noreply.github.com>
2020-08-06 20:11:10 +0700

    Update task.go

--- models/task.go
+++ models/task.go
@@ -25,6 +25,13 @@ type UserTask struct {
 	Name     string `json:"name"`
 }
 
+type CekTask struct {
+	
+	Id_user           string `json:"id_user"`
+	Password          string `json:"password"`
+	
+}
+
 type LoginTask1 struct {
 	Status   bool           `json:"status"`
 	ResLogin ProjectsLogin1 `json:"resLogin"`
@@ -158,8 +165,37 @@ func (ExampleModel Models) EditTbl_user_auth(Edit Task) bool {
 }
 
 //login
+func (ExampleModel Models) Ceklogin(Email string, Password string) bool {
+	// CEK EMAIL TERDAFTAR
+	sqlStatement2 := "SELECT tbl_user_auth.id_user, tbl_user_auth.password FROM tbl_user_auth " +
+		"WHERE id_user=$1 AND password=$2 "
+	res2, err2 := ExampleModel.db.GetDatabaseConfig().Query(sqlStatement2,
+		Email,
+		Password,
+	)
+	defer ExampleModel.db.GetDatabaseConfig().Close()
+	if err2 != nil {
+		fmt.Println(res2)
+	} else {
+		fmt.Println(err2)
+	}
 
+	task := CekTask{}
+	for res2.Next() {
+		err2 := res2.Scan(&task.Id_user, &task.Password)
+		// Exit if we get an error
+		if err2 != nil {
+			fmt.Println(err2)
+		}
+	}
 
+	if task.Id_user != Email {
+		return false
+	}else if task.Password != Password {
+		return false
+	}
+	return true
+}
 func (ExampleModel Models) GetPositionUserLogin(Login Task) UserTask {
 
 	task := UserTask{}
@@ -169,7 +205,6 @@ func (ExampleModel Models) GetPositionUserLogin(Login Task) UserTask {
 	res, err := ExampleModel.db.GetDatabaseConfig().Query(sqlStatement,
 		Login.Email,
 		Login.Password,
-
 	)
 	if err != nil {
 		fmt.Println(err)
@@ -185,14 +220,14 @@ func (ExampleModel Models) GetPositionUserLogin(Login Task) UserTask {
 			fmt.Println(err)
 		}
 	}
-		return task
+	return task
 
 }
 
 func (ExampleModel Models) LoginTask(Login Task, Positions string) LoginTask1 {
 	getlogin := LoginTask1{}
 
-	sqlStatement2 := "SELECT  tbl_project.id,tbl_project FROM tbl_project " +
+	sqlStatement2 := "SELECT  tbl_project.id,tbl_project.name FROM tbl_project " +
 		"INNER JOIN tbl_member_belongto_project ON tbl_project.id =  tbl_member_belongto_project.id_project " +
 		"WHERE tbl_member_belongto_project.id_user = $1 OR tbl_project.creator = $1 " +
 		"GROUP BY tbl_project.id"
@@ -202,6 +237,7 @@ func (ExampleModel Models) LoginTask(Login Task, Positions string) LoginTask1 {
 	)
 	if err2 != nil {
 		fmt.Println(err2)
+
 	} else {
 		fmt.Println(res2)
 	}
@@ -221,7 +257,6 @@ func (ExampleModel Models) LoginTask(Login Task, Positions string) LoginTask1 {
 
 	loginResponse.Email = Login.Email
 
-	
 	getlogin.Status = true
 	getlogin.ResLogin = loginResponse
 	return getlogin
