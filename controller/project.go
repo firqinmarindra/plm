7e16444b8845aa31cf176b02243f8b91454cfad2
firqinmarindra <68682942+firqinmarindra@users.noreply.github.com>
2020-08-06 23:04:06 +0700

    Update project.go

--- controller/project.go
+++ controller/project.go
@@ -23,18 +23,23 @@ type TaskProject struct {
 //}
 
 type EditGet struct {
-	Email 			string `json:"email"`
-	Project_name 	string `json:"project_name"`
-	Deskripsi 		string `json:"deskripsi"`
-	Id_project 		int `json:"id_project"`
+	Email        string `json:"email"`
+	Project_name string `json:"project_name"`
+	Deskripsi    string `json:"deskripsi"`
+	Id_project   int    `json:"id_project"`
 }
 
-type  CreateGets struct {
-	Id_project 		int `json:"id_project"`
-	Project_name 	string `json:"project_name"`
-	Deskripsi 		string `json:"deskripsi"`
-
+type CreateGets struct {
+	Id_project   int    `json:"id_project"`
+	Project_name string `json:"project_name"`
+	Deskripsi    string `json:"deskripsi"`
 }
+
+type ProjectViewget struct {
+	Email      string `json:"email"`
+	Id_project int    `json:"id_project"`
+}
+
 //create
 func (Controller Controller) PostsCreate(c echo.Context) error {
 	a := new(TaskProject)
@@ -46,40 +51,37 @@ func (Controller Controller) PostsCreate(c echo.Context) error {
 		Email:        a.Email,
 		Project_name: a.Project_name,
 		Deskripsi:    a.Deskripsi,
-
 	}
 
 	fmt.Println(a.Email)
 
-
-
 	insertTbl_project := Controller.ma.InsertTbl_Project(ab)
 	GetMaxId := Controller.ma.MaxIdProject(ab)
 	//cekIdProject := Controller.ma.MaxIdProject(ab)
-	insertTbl_member_belongto_project := Controller.ma.InsertTbl_member_belongto_project(ab,GetMaxId)
+	insertTbl_member_belongto_project := Controller.ma.InsertTbl_member_belongto_project(ab, GetMaxId)
 
 	fmt.Println(GetMaxId)
 	//if cekIdProject {
 
-		if insertTbl_project && insertTbl_member_belongto_project {
+	if insertTbl_project && insertTbl_member_belongto_project {
 
-			getCreate := CreateGets{Id_project: GetMaxId, Project_name: a.Project_name, Deskripsi: a.Deskripsi}
-			res := responsegenr.ResponseGenericGet{
-				Status:  "Success",
-				Message: "Berhasil create project",
-				Data:    getCreate,
-			}
-			return c.JSON(http.StatusOK, res)
-
-		} else {
-
-			res := responsegenr.ResponseGeneric{
-				Status:  "Error",
-				Message: "Gagal create project",
-			}
-			return c.JSON(http.StatusOK, res)
-
+		getCreate := CreateGets{Id_project: GetMaxId, Project_name: a.Project_name, Deskripsi: a.Deskripsi}
+		res := responsegenr.ResponseGenericGet{
+			Status:  "Success",
+			Message: "Berhasil create project",
+			Data:    getCreate,
 		}
+		return c.JSON(http.StatusOK, res)
+
+	} else {
+
+		res := responsegenr.ResponseGeneric{
+			Status:  "Error",
+			Message: "Gagal create project",
+		}
+		return c.JSON(http.StatusOK, res)
+
+	}
 	//} else {
 	//
 	//	res := responsegenr.ResponseGeneric{
@@ -102,13 +104,13 @@ func (Controller Controller) EditProject(c echo.Context) error {
 		Email:        a.Email,
 		Project_name: a.Project_name,
 		Deskripsi:    a.Deskripsi,
-		Id_project: 	a.Id_project,
+		Id_project:   a.Id_project,
 	}
 
 	cekId_Project := Controller.ma.CekId_Project(ab)
 	editTbl_Project := Controller.ma.EditTbl_Project(ab)
 
-	if cekId_Project{
+	if cekId_Project {
 
 		if editTbl_Project {
 
@@ -159,4 +161,3 @@ func (Controller Controller) ViewProject(c echo.Context) error {
 	return c.JSON(http.StatusOK, res)
 
 }
-
