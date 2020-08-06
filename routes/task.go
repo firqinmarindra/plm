14c45f613b32d4ef4ab69c0d79925566ae6e2496
firqinmarindra <68682942+firqinmarindra@users.noreply.github.com>
2020-08-06 20:26:56 +0700

    Update task.go

--- routes/task.go
+++ routes/task.go
@@ -15,11 +15,15 @@ type RoutingInterface interface {
 
 func (Routing Routing) GetRoutes() *echo.Echo {
 	e := echo.New()
+	
+	//user
 	e.POST("/tasks/user/register/", Routing.routes.PostsRegis)
 	e.POST("/tasks/user/login/", Routing.routes.GetLogin)
 	e.PUT("/tasks/user/edit/", Routing.routes.UpEdit)
+
+	//project
 	e.POST("/tasks/project/create/", Routing.routes.PostsCreate)
-	//e.GET("/tasks/project/view/", Routing.routes.ViewProject)
+	e.POST("/tasks/project/view/", Routing.routes.ViewProject)
 	e.PUT("/tasks/project/edit/", Routing.routes.EditProject)
 
 	return e
